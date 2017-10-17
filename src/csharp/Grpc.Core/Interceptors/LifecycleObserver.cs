8cfe46e014ea02d36da3ba129e537cbe16c13c59
Mehrdad Afshari <mmx@google.com>
2017-10-17 14:11:09 -0700

    wip

--- src/csharp/Grpc.Core/Interceptors/LifecycleObserver.cs
+++ src/csharp/Grpc.Core/Interceptors/LifecycleObserver.cs
@@ -31,90 +31,6 @@ namespace Grpc.Core.Interceptors
     public class LifecycleObserverInterceptor : Interceptor
     {
         /// <summary>
-        /// Intercepts a blocking invocation of a simple remote call.
-        /// </summary>
-        /// <param name="request">The request message of the invocation.</param>
-        /// <param name="context">
-        /// The <see cref="Grpc.Core.Interceptors.ClientInterceptorContext{TRequest, TResponse}"/>
-        /// associated with the current invocation.
-        /// </param>
-        /// <param name="continuation">
-        /// The callback that continues the invocation process.
-        /// This can be invoked zero or more times by the interceptor.
-        /// </param>
-        /// <returns>The response messaage of the current invocation.</returns>
-        public override TResponse BlockingUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
-        {
-            return continuation(request, context);
-        }
-
-        /// <summary>
-        /// Intercepts an asynchronous invocation of a simple remote call.
-        /// </summary>
-        /// <param name="request">The request message of the invocation.</param>
-        /// <param name="context">
-        /// The <see cref="Grpc.Core.Interceptors.ClientInterceptorContext{TRequest, TResponse}"/>
-        /// associated with the current invocation.
-        /// </param>
-        /// <param name="continuation">
-        /// The callback that continues the invocation process.
-        /// This can be invoked zero or more times by the interceptor.
-        /// </param>
-        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
-        {
-            return continuation(request, context);
-        }
-
-        /// <summary>
-        /// Intercepts an asynchronous invocation of a streaming remote call.
-        /// </summary>
-        /// <param name="request">The request message of the invocation.</param>
-        /// <param name="context">
-        /// The <see cref="Grpc.Core.Interceptors.ClientInterceptorContext{TRequest, TResponse}"/>
-        /// associated with the current invocation.
-        /// </param>
-        /// <param name="continuation">
-        /// The callback that continues the invocation process.
-        /// This can be invoked zero or more times by the interceptor.
-        /// </param>
-        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            return continuation(request, context);
-        }
-
-        /// <summary>
-        /// Intercepts an asynchronous invocation of a client streaming call.
-        /// </summary>
-        /// <param name="context">
-        /// The <see cref="Grpc.Core.Interceptors.ClientInterceptorContext{TRequest, TResponse}"/>
-        /// associated with the current invocation.
-        /// </param>
-        /// <param name="continuation">
-        /// The callback that continues the invocation process.
-        /// This can be invoked zero or more times by the interceptor.
-        /// </param>
-        public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context, AsyncClientStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            return continuation(context);
-        }
-
-        /// <summary>
-        /// Intercepts an asynchronous invocation of a duplex streaming call.
-        /// </summary>
-        /// <param name="context">
-        /// The <see cref="Grpc.Core.Interceptors.ClientInterceptorContext{TRequest, TResponse}"/>
-        /// associated with the current invocation.
-        /// </param>
-        /// <param name="continuation">
-        /// The callback that continues the invocation process.
-        /// This can be invoked zero or more times by the interceptor.
-        /// </param>
-        public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context, AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            return continuation(context);
-        }
-
-        /// <summary>
         /// Server-side handler for intercepting unary calls.
         /// </summary>
         /// <typeparam name="TRequest">Request message type for this method.</typeparam>
@@ -203,7 +119,7 @@ namespace Grpc.Core.Interceptors
                     return handler;
                 }
 
-                return null;
+                return handler;
             };
         }
 
